1ce7c0a144ff51ee0ec8b1d3a96ee9f031838629
OfentseXI <dofentse69@gmail.com>
2025-10-14 03:01:20 +0200

    `Update Firebase configuration in client and lib files with hardcoded values.`

--- src/integrations/firebase/client.ts
+++ src/integrations/firebase/client.ts
@@ -4,12 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC-R2EKBnULSLL80-vGZNtVagYFDfGy5Xg",
+  authDomain: "salema-dce69.firebaseapp.com",
+  databaseURL: "https://salema-dce69-default-rtdb.firebaseio.com",
+  projectId: "salema-dce69",
+  storageBucket: "salema-dce69.appspot.com",
+  messagingSenderId: "486960310117",
+  appId: "1:486960310117:web:dda312b600a939afbbe38c",
+  measurementId: "G-7QK3CV70H6",
 };
 
 const app = initializeApp(firebaseConfig);
